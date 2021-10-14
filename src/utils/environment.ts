d06b7f41c25760f957a1a91a109c70fded2aa0e8
Cory Cooper <coopercory92@gmail.com>
2021-10-13 22:12:31 -0700

    ci: Backend env var

--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -1,6 +1,5 @@
-export const ENVIRONMENT = import.meta.env.VITE_ENVIRONMENET;
+export const ENVIRONMENT = import.meta.env.VITE_ENVIRONMENET as string;
 
-export const BACKEND_HOST: string =
-	ENVIRONMENT === 'production' ? 'https://api.appmasker.com' : 'http://localhost:3000';
+export const BACKEND_HOST = import.meta.env.VITE_BACKEND as string;
 
 export const BUSINESS_STANDARD_PRICE_ID = import.meta.env.VITE_BUSINESS_PLAN_PRICE_ID as string;
